afb49175c38dcddd9d2a32725af2c8dce3b18e8e
Filippo B <filippo82@users.noreply.github.com>
2019-12-12 14:26:39 +0100

    Add error page

--- flask_essential_training_linkedin/url_shortener/app.py
+++ flask_essential_training_linkedin/url_shortener/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -34,7 +34,7 @@ def your_url():
         else:
             f = request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
-            f.save('/work/bfilippo/learning_flask/flask_essential_training_linkedin/url_shortener/' + full_name)
+            f.save('/work/bfilippo/learning_flask/flask_essential_training_linkedin/url_shortener/static/user_files/' + full_name)
             urls[request.form['code']] = {'file': full_name}
         # Save the urls to a JSON file
         with open('urls.json', 'w') as url_file:
@@ -42,3 +42,20 @@ def your_url():
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    # Check for existing JSON file
+    if os.path.exists('urls.json'):
+        with open('urls.json') as url_file:
+            urls = json.load(url_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
+                else:
+                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
